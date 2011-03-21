9660a3aad24144d9a61ce39e6f0b71b07dff809f
Edoardo Sabadelli <edoardo@sabadelli.it>
2011-03-21 10:41:25 +0100

    Pass the app mode in the stash. Added Google Analytics only in production mode. Added generic e-mail address in contacts section.

--- app.pl
+++ app.pl
@@ -7,15 +7,21 @@ plugin 'tt_renderer' => {template_options => {ENCODING => 'utf8'}};
 plugin 'i18n' => {namespace => 'ES::Web::SabadelliDotIt::Edoardo::I18N'};
 
 get '/edoardo/wedding' => sub {
-  my $self = shift;
+    my $self = shift;
 
-  $self->render('index');
+    $self->render(
+        'index',
+        app => {mode => app->mode},
+    );
 };
 
 get '/edoardo/wedding/for-our-friends-from-abroad' => sub {
     my $self = shift;
 
-    $self->render('information');
+    $self->render(
+        'information',
+        app => {mode => app->mode},
+    );
 };
 
 app->start;

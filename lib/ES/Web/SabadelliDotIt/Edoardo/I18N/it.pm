c8254bd0929d11c2597acf823de1432d0ce71d8c
Edoardo Sabadelli <edoardo@sabadelli.it>
2011-03-22 22:59:40 +0100

    Italian translation for the main page!

--- lib/ES/Web/SabadelliDotIt/Edoardo/I18N/it.pm
+++ lib/ES/Web/SabadelliDotIt/Edoardo/I18N/it.pm
@@ -2,26 +2,99 @@ package ES::Web::SabadelliDotIt::Edoardo::I18N::it;
 
 use strict;
 
+use utf8;
+
 use Mojo::Base 'ES::Web::SabadelliDotIt::Edoardo::I18N';
 
 our %Lexicon = (
-    'Margaux and Edoardo' => 'Margaux e Edoardo',
-    q(Margaux and Edoardo's wedding) => 'Il matrimonio di Margaux e Edoardo',
-    'are getting married!' => 'si sposano',
-    '30 September 2011' => '30 settembre 2011',
-    'Wedding' => 'Matrimonio',
-    'Ceremony' => 'Cerimonia',
-    'Reception' => 'Ricevimento',
-    'Map and driving directions' => 'Mappa e direzioni di guida',
-    'About us' => 'Noi',
-    'For our friends from abroad' => q(Per gli amici dall'estero),
-    'Contact us' => 'Contatti',
+
+# Common header
+q(Margaux and Edoardo's wedding) => 'Il matrimonio di Margaux e Edoardo',
+'Margaux and Edoardo' => 'Margaux e Edoardo',
+'30 September 2011' => '30 settembre 2011',
+
     'Flights' => 'Voli',
     'Flight search' => 'Ricerca voli',
     'Accommodation' => 'Alloggio',
     'Transportation' => 'Mezzi di trasporto',
     'Italy' => 'Italia',
     'Norway' => 'Norvegia',
+
+# Wedding
+'Wedding' => 'Matrimonio',
+
+'Ceremony' => 'Cerimonia',
+q(The ceremony will take place in <span class="location"><a href="http://tramonto-venue.co.za/the_chapel.html" class="url">the Chapel</a> at <a href="http://www.tramonto-venue.co.za/">Tramonto</a></span>, starting at <time datetime="2011-09-30T16:00+02:00" class="dtstart" title="2011-09-30T16:00+02:00">16:00 <abbr title="South Africa Standard Time">SAST</abbr></time> sharp.) =>
+q(La cerimonia si terrà presso <span class="location"><a href="http://tramonto-venue.co.za/the_chapel.html" class="url">la Cappella</a> a <a href="http://www.tramonto-venue.co.za/">Tramonto</a></span>, con inizio alle ore <time datetime="2011-09-30T16:00+02:00" class="dtstart" title="2011-09-30T16:00+02:00">16:00 <abbr title="South Africa Standard Time">SAST</abbr></time> in punto.),
+
+'Reception' => 'Ricevimento',
+q(The reception will be held in <span class="location"><a href="http://www.tramonto-venue.co.za/the_venue.html" class="url">the Venue</a> also at <a href="http://www.tramnto-venue.co.za/">Tramonto</a></span>, starting approximately at <time datetime="2011-09-30T19:00+02:00" class="dtstart" title="2011-09-30T19:00+02:00">19:00 <abbr title="South Africa Standard Time">SAST</abbr></time>.) =>
+q(Il ricevimento si terrà presso <span class="location"><a href="http://www.tramonto-venue.co.za/the_venue.html" class="url">la Venue</a> sempre a <a href="http://www.tramnto-venue.co.za/">Tramonto</a></span>, con inizio approssimativamente alle ore <time datetime="2011-09-30T19:00+02:00" class="dtstart" title="2011-09-30T19:00+02:00">19:00 <abbr title="South Africa Standard Time">SAST</abbr></time>.),
+
+q(Buffet dinner will be served, with music and dancing to follow.) =>
+q(Verrà servita la cena a buffet, accompagnata da buona musica, con balli a seguire.),
+
+'Map and driving directions' => 'Mappa e direzioni di guida',
+q(The following map shows the area around Tramonto in George.) =>
+q(La mappa seguente mostra l'area intorno a Tramonto a George.),
+
+q(If you're driving and have a <abbr title="Global Positioning System">GPS</abbr>, you can use these coordinates to reach the location: <span class="latitude" title="-033.9644444">S 33° 57' 52"</span> - <span class="longitude" title="022.3736111">E 22° 22' 25"</span>.) =>
+q(Nel caso venite in auto, e avete un dispositivo <abbr title="Global Positioning System">GPS</abbr>, potete usare queste coordinate per raggiungere il posto: <span class="latitude" title="-033.9644444">S 33° 57' 52"</span> - <span class="longitude" title="022.3736111">E 22° 22' 25"</span>.),
+
+q(There's parking space on the premises.) =>
+q(Un'area di parcheggio è disponibile sul posto.),
+
+q(View in a larger map) =>
+q(Visualizza una mappa più grande),
+
+# About us
+'About us' => 'Noi',
+
+q(We first met in Oslo sometime in July 2007, when Margaux worked for a couple of weeks at Opera Software as a summer intern. Annchen played a role here) =>
+q(Ci siamo incontrati per la prima volta a Oslo nel luglio 2007, quando Margaux ha lavorato per un paio di settimane a Opera Software. Annchen ha giocato un ruolo qui),
+
+q(After that, apart for being friends on Facebook, we didn't see each other for almost a whole year, until June 2008, when Edoardo got the brilliant idea of asking me out, once he was back from a vacation in his homeland.) =>
+q(Dopo quel giorno, a parte essere diventati amici su Facebook, non ci siamo più visti per quasi un anno, fino al giugno 2008, quando Edoardo ha avuto la brillante idea di chiedermi di uscire con lui, una volta di ritorno da una vacanza in Italia.),
+
+q(From that day, we kept in touch while Edoardo was roaming around Norway.) =>
+q(Da quel giorno abbiamo mantenuto i contatti mentre Edoardo viaggiava in giro per la Norvegia.),
+
+q(We eventually started our relationship in July 2008.) =>
+q(Abbiamo iniziato la nostra relazione nel luglio 2008.),
+
+q(In November Margaux moved north to Stjørdal for work, but even with more than 500 km separating us and countless flights across Norway, the story continued.) =>
+q(In novembre Margaux si è trasferita a Stjørdal per lavoro, ma anche con più di 500 km a separarci e innumerevoli voli attraverso la Norvegia, la storia è continuata.),
+
+q(We got engaged in July 2010 and Margaux moved back to Oslo a few months later.) =>
+q(Ci siamo fidanzati nel luglio 2010 e Margaux è ritornata a Oslo qualche mese dopo.),
+
+# For our friends from abroad
+'For our friends from abroad' => q(Per gli amici dall'estero),
+
+q(We've collected some information on a <a href="/edoardo/wedding/for-our-friends-from-abroad">separate page</a> about flights, accommodation and touristic attractions in the area.) =>
+q(Abbiamo raccolto un pò di informazioni in una <a href="/edoardo/wedding/for-our-friends-from-abroad">pagina a parte</a> riguardo voli, alloggi e attrazioni turistiche nell'area.),
+
+q(We hope you'll have a nice time in South Africa while sharing this special day with us!) =>
+q(Speriamo che possiate passare un'ottima vacanza in Sud Africa oltre a condividere con noi questo giorno speciale!),
+
+# Contact us
+'Contact us' => 'Contatti',
+
+q(For any request, suggestion, comment you can write us at) =>
+q(Per ogni richiesta, suggerimento, commento potete scriverci a),
+
+q(Download the business card) =>
+q(Scarica il biglietto da visita),
+
+q(Download vCard) =>
+q(Scarica la vCard),
+
+q(Scan the barcode with your phone's camera) =>
+q(Leggi il barcode con la fotocamera del cellulare),
+
+q(Add to your mobile phone) =>
+q(Aggiungi al cellulare),
+
 );
 
 1;

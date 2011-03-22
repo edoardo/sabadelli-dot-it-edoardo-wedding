4b0b74e0e2dfcb9b43b352cbe1a770d68c8d371e
Edoardo Sabadelli <edoardo@sabadelli.it>
2011-03-22 21:37:37 +0100

    Store the language in a cookie and read it back from it.

--- app.pl
+++ app.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+# http://www.sabadelli.it/edoardo/wedding website :)
+
 use Mojolicious::Lite;
 
 use lib 'lib';
@@ -8,32 +10,41 @@ plugin 'charset' => {charset => 'utf-8'};
 plugin 'tt_renderer';
 plugin 'i18n' => {namespace => 'ES::Web::SabadelliDotIt::Edoardo::I18N'};
 
-my $lang = 'en';
+app->secret('wglisposi!');
 
 app->defaults(
     app => {
         mode => app->mode,
-        lang => $lang,
     },
 );
 
 get '/edoardo/wedding' => sub {
     my $self = shift;
 
-    if (my $lang = $self->param('lang')) {
-        $self->stash->{app}->{lang} = $lang;
+    my $lang = $self->session('lang');
+
+    if ($self->param('lang')) {
+        $lang = $self->param('lang');
+
+        $self->session(lang => $lang);
     }
 
+    $self->stash->{app}->{lang} = $lang || 'en';
     $self->render('index');
 };
 
 get '/edoardo/wedding/for-our-friends-from-abroad' => sub {
     my $self = shift;
 
-    if (my $lang = $self->param('lang')) {
-        $self->stash->{app}->{lang} = $lang;
+    my $lang = $self->session('lang');
+
+    if ($self->param('lang')) {
+        $lang = $self->param('lang');
+
+        $self->session(lang => $lang);
     }
 
+    $self->stash->{app}->{lang} = $lang || 'en';
     $self->render('information');
 };
 

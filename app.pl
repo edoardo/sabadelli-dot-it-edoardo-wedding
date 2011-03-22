e3911e522a9e69044310428715b5b4c31d27de75
Edoardo Sabadelli <edoardo@sabadelli.it>
2011-03-22 18:19:05 +0100

    Added support for I18N. Started translating in Italian. Added a link in the footer for changing language.
    TODO: set the language in a cookie and read it from there.

--- app.pl
+++ app.pl
@@ -2,26 +2,39 @@
 
 use Mojolicious::Lite;
 
+use lib 'lib';
+
 plugin 'charset' => {charset => 'utf-8'};
-plugin 'tt_renderer' => {template_options => {ENCODING => 'utf8'}};
+plugin 'tt_renderer';
 plugin 'i18n' => {namespace => 'ES::Web::SabadelliDotIt::Edoardo::I18N'};
+
+my $lang = 'en';
+
+app->defaults(
+    app => {
+        mode => app->mode,
+        lang => $lang,
+    },
+);
 
 get '/edoardo/wedding' => sub {
     my $self = shift;
 
-    $self->render(
-        'index',
-        app => {mode => app->mode},
-    );
+    if (my $lang = $self->param('lang')) {
+        $self->stash->{app}->{lang} = $lang;
+    }
+
+    $self->render('index');
 };
 
 get '/edoardo/wedding/for-our-friends-from-abroad' => sub {
     my $self = shift;
 
-    $self->render(
-        'information',
-        app => {mode => app->mode},
-    );
+    if (my $lang = $self->param('lang')) {
+        $self->stash->{app}->{lang} = $lang;
+    }
+
+    $self->render('information');
 };
 
 app->start;
---
+++ lib/ES/Web/SabadelliDotIt/Edoardo/I18N/it.pm
@@ -0,0 +1,27 @@
+package ES::Web::SabadelliDotIt::Edoardo::I18N::it;
+
+use strict;
+
+use Mojo::Base 'ES::Web::SabadelliDotIt::Edoardo::I18N';
+
+our %Lexicon = (
+    'Margaux and Edoardo' => 'Margaux e Edoardo',
+    q(Margaux and Edoardo's wedding) => 'Il matrimonio di Margaux e Edoardo',
+    'are getting married!' => 'si sposano',
+    '30 September 2011' => '30 settembre 2011',
+    'Wedding' => 'Matrimonio',
+    'Ceremony' => 'Cerimonia',
+    'Reception' => 'Ricevimento',
+    'Map and driving directions' => 'Mappa e direzioni di guida',
+    'About us' => 'Noi',
+    'For our friends from abroad' => q(Per gli amici dall'estero),
+    'Contact us' => 'Contatti',
+    'Flights' => 'Voli',
+    'Flight search' => 'Ricerca voli',
+    'Accommodation' => 'Alloggio',
+    'Transportation' => 'Mezzi di trasporto',
+    'Italy' => 'Italia',
+    'Norway' => 'Norvegia',
+);
+
+1;
